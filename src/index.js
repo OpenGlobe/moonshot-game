c5b5ac0de9a2acfca1227c236f4ef5f8cb87f784
Chris Goranson <openglobe@gmail.com>
2025-05-23 23:40:27 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import MoonshotGame from './App';
+import App from './App';
+import { ThemeProvider } from './ThemeContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <MoonshotGame />
-  </React.StrictMode>
+  <ThemeProvider>
+    <App />
+  </ThemeProvider>
 );
+
