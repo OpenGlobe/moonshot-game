7adf6dff6e77c2cd73e3e1f6489c53dd1ae5bebb
Chris Goranson <openglobe@gmail.com>
2025-05-23 23:00:32 -0400

    Create themes.js

---
+++ src/themes.js
@@ -0,0 +1,18 @@
+// themes.js
+export const defaultTheme = {
+  background: '#0b0c2a',
+  color: '#e0e0ff',
+  buttonBackground: '#00ffe7',
+  buttonColor: '#000',
+  cardBackground: '#1a1a40',
+  borderColor: '#7df9ff',
+};
+
+export const retroTheme = {
+  background: '#000',
+  color: '#00ff00',
+  buttonBackground: '#ff00ff',
+  buttonColor: '#fff',
+  cardBackground: '#111',
+  borderColor: '#ff00ff',
+};
